254e58ee67490263a2133b8d0e2a2a34765dc7a3
Lucas1196 <45823230+Lucas1196@users.noreply.github.com>
2018-12-18 17:02:13 +0200

    Update map.js

--- map.js
+++ map.js
@@ -1,71 +1,79 @@
 var board = [ 
-    [' ',' ',' ',' ',' ',' ',' ',' '],
-    [' ','2',' ',' ',' ',' ',' ',' '],
-    [' ',' ',' ',' ',' ',' ',' ',' '],
-    [' ',' ',' ',' ','w',' ',' ',' '],
-    [' ',' ',' ',' ',' ',' ',' ',' '],
-    [' ',' ',' ',' ',' ',' ',' ',' '],
-    ['s',' ',' ',' ',' ',' ',' ','w'],
-    ['s',' ',' ',' ',' ',' ',' ','w']
+    [0, 1, 0, 1, 0, 1, 0, 1],
+    [0, 1, 0, 1, 0, 1, 0, 1],
+    [0, 1, 0, 1, 0, 1, 0, 1],
+    [0, 1, 0, 1, 0, 1, 0, 1],
+    [0, 1, 0, 1, 0, 1, 0, 1],
+    [0, 1, 0, 1, 0, 1, 0, 1],
+    [0, 1, 0, 1, 0, 1, 0, 1],
+    [0, 1, 0, 1, 0, 1, 0, 1]
 ];
 
 var table = document.getElementById ("table");
 
 function determinateColor() {
-    if (y % 2 == 0 && x % 2 !== 0) {
+    if (y % 2 == 0 && x % 2 != 0) {
         cell.classList.add("black");
     }
-    else if (y % 2 !==0 && x % 2 == 0) {
+    else if (y % 2 != 0 && x % 2 == 0) {
         cell.classList.add("black");
     }
-    else if (y % 2 == 0 && x % 2 == 0) {
-        cell.classList.add("white");
-    }
+    // else if (y % 2 == 0 && x % 2 == 0) {
+    //     cell.classList.add("white");
+    // }
     else {
         cell.classList.add("white");
     }
 }
 
-//Coordonates for Piece(DYNAMIC)
-var positionX = 3;
-var positionY = 0;
+
+    let positionX = 3;
+    let positionY = 2;   
+
+
 
 //Columns
 for ( var y = 0; y < board.length; y++ ) { 
     var column = table.appendChild(document.createElement("div"));
         column.classList.add("column");
-    //Row
+        column.dataset.y = y;
+    //Cell
     for ( var x = 0; x < board.length; x++ ) {
         var cell = column.appendChild(document.createElement("span"));
+            cell.dataset.x = x;
             cell.addEventListener("click", function(e) {
                 if ( e.target == piece ) {
                     console.log("I am the piece");
-                }
-                else if ( e.target !== piece && e.target.contains(piece) ) {
-                    console.log("I am the box of the piece");
+                    return;
                 }
                 else if ( !e.target.contains(piece) ) {
-                    console.log("I am ready for you");
-                    // var newPiece = e.target.appendChild(document.createElement("b"));
-                    //     newPiece.classList.add("pawn");
-                    // if ( e.target.offsetParent.nextSibling.classList.contains("piece-inside") && !e.target.offsetParent.classList.contains("piece-inside") ) {
-                    //     console.log("ready");
-                    // }
+                    console.log("I am the cell without piece");
+                    move();
+                    return;
                 }
             })
-        determinateColor();
-        //Condition for Coordonates(piece)
-        if ( positionX == x && positionY == y ) {
-            var piece = cell.appendChild(document.createElement("b"));
-                piece.classList.add("pawn");
-                column.classList.add("piece-inside");
-        }
-     }
+            if ( positionX == x && positionY == y ) {
+                var piece = cell.appendChild(document.createElement("b"));
+                    piece.classList.add("pawn");
+                    column.classList.add("piece-inside");
+                    // console.log(document.getElementsByClassName("piece-inside"));
+            }
+            determinateColor();
+            position();
+    }
 }
 
-// piece.addEventListener("click", function(e) {
-    // e.stopPropagation();
-    // e.cancelBubble = true;
-// })
 
-https://jsfiddle.net/3mnLcs9h/
+// else if ( e.target !== piece && e.target.contains(piece) ) {
+//     console.log("I am the cell with piece");
+//     // console.log(e.target);
+//     return;
+// }
+
+// else if ( e.target.offsetParent.previousSibling.classList.contains("piece-inside") || e.target.offsetParent.nextSibling.classList.contains("piece-inside") && !e.target.contains(piece) ) {
+//     this.offsetParent.previousSibling.classList.remove("piece-inside");
+//     this.offsetParent.classList.add("piece-inside");
+//     var piece = e.target.appendChild(document.createElement("b"));
+//         piece.classList.add("pawn");
+//         console.log("I am the cell without piece");
+// }
